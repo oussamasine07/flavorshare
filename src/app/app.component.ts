b40950f271d0c746f4c0ec87b811b15fd1f7a912
oussamasine07 <sineoussama@gmail.com>
2025-04-30 17:16:54 +0100

    finish hero section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './components/partials/navbar/navbar.component';
+import { HeroSectionComponent } from './components/partials/hero-section/hero-section.component';
 
 @Component({
   selector: 'app-root',
   imports: [
     RouterOutlet,
-  NavbarComponent
+    NavbarComponent, HeroSectionComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
