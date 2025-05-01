e91ced1a359608742fe0bb5abbe890102877a24e
sine oussama <sineoussama@gmail.com>
2025-05-01 18:27:29 +0100

    add heading section component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,13 @@ import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './components/partials/navbar/navbar.component';
 import { HeroSectionComponent } from './components/partials/hero-section/hero-section.component';
 import { CardWrapperComponent } from './components/partials/card-wrapper/card-wrapper.component';
+import { SectionHeadingComponent } from './components/partials/section-heading/section-heading.component';
 
 @Component({
   selector: 'app-root',
   imports: [
     RouterOutlet,
-    NavbarComponent, HeroSectionComponent, CardWrapperComponent
+    NavbarComponent, HeroSectionComponent, SectionHeadingComponent, CardWrapperComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
