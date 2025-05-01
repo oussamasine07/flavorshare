c16921b486ba6afafefb9bae8ffd1a69ecc0c3b2
sine oussama <sineoussama@gmail.com>
2025-05-01 20:05:33 +0100

    finish card functionality

--- src/app/components/partials/card-wrapper/card-wrapper.component.ts
+++ src/app/components/partials/card-wrapper/card-wrapper.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { RecipeCardComponent } from '../recipe-card/recipe-card.component';
 
 @Component({
   selector: 'app-card-wrapper',
-  imports: [],
+  imports: [
+    RecipeCardComponent
+  ],
   templateUrl: './card-wrapper.component.html',
   styleUrl: './card-wrapper.component.css'
 })
