49c4e9aa9b1c63949dc36a7b56e14f06a9fece2f
oussamasine07 <sineoussama@gmail.com>
2025-05-02 10:50:09 +0100

    finish detail page layout

--- src/app/components/pages/recipe-details/recipe-details.component.ts
+++ src/app/components/pages/recipe-details/recipe-details.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { SectionHeadingComponent } from '../../partials/section-heading/section-heading.component';
 
 @Component({
   selector: 'app-recipe-details',
-  imports: [],
+  imports: [
+    SectionHeadingComponent
+  ],
   templateUrl: './recipe-details.component.html',
   styleUrl: './recipe-details.component.css'
 })
