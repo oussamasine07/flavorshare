baaa6ec5db176a96ebcb50961d3d9a24bc615d62
oussamasine07 <sineoussama@gmail.com>
2025-05-02 09:58:59 +0100

    generate recipe details page component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './components/partials/navbar/navbar.component';
-import { HeroSectionComponent } from './components/partials/hero-section/hero-section.component';
-import { CardWrapperComponent } from './components/partials/card-wrapper/card-wrapper.component';
-import { SectionHeadingComponent } from './components/partials/section-heading/section-heading.component';
 import { FooterComponent } from './components/partials/footer/footer.component';
 
 @Component({
---
+++ src/app/components/pages/recipe-details/recipe-details.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-recipe-details',
+  imports: [],
+  templateUrl: './recipe-details.component.html',
+  styleUrl: './recipe-details.component.css'
+})
+export class RecipeDetailsComponent {
+
+}
