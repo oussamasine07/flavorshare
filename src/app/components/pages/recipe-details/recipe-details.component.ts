54c72b0463987316e7b07e05f3660ebdfc8d52c3
sine oussama <sineoussama@gmail.com>
2025-05-03 18:46:41 +0100

    finish recipe details page

--- src/app/components/pages/recipe-details/recipe-details.component.ts
+++ src/app/components/pages/recipe-details/recipe-details.component.ts
@@ -4,10 +4,14 @@ import { ActivatedRoute } from '@angular/router';
 import { RecipeService } from '../../../services/recipe/recipe.service';
 import { consumerPollProducersForChange } from '@angular/core/primitives/signals';
 import { Meal } from '../../../interfaces/meal';
+import { NgFor } from '@angular/common';
+import { YtIdPipe } from '../../../cutomPipes/youtube/yt-id.pipe';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-recipe-details',
   imports: [
+    NgFor, YtIdPipe,
     SectionHeadingComponent
   ],
   templateUrl: './recipe-details.component.html',
@@ -17,16 +21,29 @@ export class RecipeDetailsComponent implements OnInit {
 
   activatedRoute = inject(ActivatedRoute);
   recipeService = inject(RecipeService);
+  sanitizer = inject(DomSanitizer);
+  safeYouTubeUrl?: SafeResourceUrl;
 
   idMeal: string | null = this.activatedRoute.snapshot.paramMap.get("idMeal");
   recipe: Meal | null = null;
+  gradiants: string[] = [];
 
 
   ngOnInit(): void {
     this.recipeService.getRecipeByIdMeal(this.idMeal).subscribe({
       next: (recipeRes) => {
+        let idx: number = 1;
         this.recipe = recipeRes.meals[0]
-        console.log(this.recipe)
+        const ytbUrl = this.recipe?.strYoutube.split("?v=")[1];
+        if (ytbUrl) {
+          this.safeYouTubeUrl = this.sanitizer.bypassSecurityTrustResourceUrl(`https://www.youtube.com/embed/${ytbUrl}`);
+          console.log(this.safeYouTubeUrl)
+        }
+        while (this.recipe[`strIngredient${idx}` as keyof Meal] !== undefined && this.recipe[`strIngredient${idx}` as keyof Meal]?.trim() !== "") {
+          this.gradiants.push(this.recipe[`strMeasure${idx}` as keyof Meal] + " " + this.recipe[`strIngredient${idx}` as keyof Meal])
+          idx++
+        }
+        
       }
     });
   }
---
+++ src/app/cutomPipes/youtube/yt-id.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { YtIdPipe } from './yt-id.pipe';
+
+describe('YtIdPipe', () => {
+  it('create an instance', () => {
+    const pipe = new YtIdPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
