233b05eeb07780641af0b41d3eacb62558a9f868
sine oussama <sineoussama@gmail.com>
2025-05-04 12:54:49 +0100

    make categories btns

--- src/app/components/pages/home/home.component.ts
+++ src/app/components/pages/home/home.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { HeroSectionComponent } from '../../partials/hero-section/hero-section.component';
 import { SectionHeadingComponent } from '../../partials/section-heading/section-heading.component';
 import { CardWrapperComponent } from '../../partials/card-wrapper/card-wrapper.component';
+import { CategoriesBtnsComponent } from '../../partials/categories-btns/categories-btns.component';
 
 @Component({
   selector: 'app-home',
   imports: [
-    HeroSectionComponent, SectionHeadingComponent, CardWrapperComponent
+    HeroSectionComponent, SectionHeadingComponent, CardWrapperComponent,
+    CategoriesBtnsComponent
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
---
+++ src/app/components/partials/categories-btns/categories-btns.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-categories-btns',
+  imports: [],
+  templateUrl: './categories-btns.component.html',
+  styleUrl: './categories-btns.component.css'
+})
+export class CategoriesBtnsComponent {
+
+}
